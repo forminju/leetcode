80cc97f5be050e65f0b5ec1fa8f5faf0bc9c4260
JEONMINJU <92534659+forminju@users.noreply.github.com>
2025-12-09 10:49:19 +0900

    Time: 67 ms (71.49%), Space: 19.1 MB (95.14%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -2,35 +2,29 @@ from collections import deque
 
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-
-        m, n = len(maze), len(maze[0])
+        m,n = len(maze), len(maze[0])
+        er,ec = entrance
         visited = [[False]*n for _ in range(m)]
-        er, ec = entrance
         visited[er][ec] = True
-
-        q = deque()
-        q.append((er,ec,0))
 
         dx = [-1,1,0,0]
         dy = [0,0,-1,1]
 
-        while q:
-            # 먼저 뽑는 거 부터 해야지!!
-            x, y, dist = q.popleft()
+        q = deque()
+        q.append((er,ec,0))
 
-            if (x==0 or x == m-1 or y == 0 or y == n-1) and [x,y]!=entrance:
+        while q:
+            x,y,dist = q.popleft()
+            if (x==0 or x==m-1 or y == 0 or y == n-1) and [x,y]!=entrance:
                 return dist
 
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if 0<=nx<m and 0<=ny<n and maze[nx][ny] == '.' and not visited[nx][ny]:
+                if 0<=nx<m and 0<=ny<n and maze[nx][ny] =='.' and visited[nx][ny] == False:
                     visited[nx][ny] = True
                     q.append((nx,ny,dist+1))
 
         return -1
-
-
-
         
